98421abf49bee3eb0f7c69472c4601273288435a
Hagen Dittmer <hagen.dittmer@outlook.com>
2015-10-15 10:44:24 +1100

    First commit

---
+++ app/routes/sms.js
@@ -0,0 +1,13 @@
+// load up the user model
+var Person = require('../models/person');
+
+module.exports = function(app) {
+  // middleware specific to this router
+  app.use(function timeLog(req, res, next) {
+    console.log('SMS lib initialised', Date.now());
+    next();
+  });
+
+  // Hand off to sms handler
+  require("../sms")(app);
+}
